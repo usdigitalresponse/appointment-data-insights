c1a9ecd41612c381eb9ac2eaf78a5c55596bd89d
Rob Brackett <rob@robbrackett.com>
2022-01-27 22:44:03 -0800

    Make parallelizable and cacheable

--- src/count_slots.py
+++ src/count_slots.py
@@ -1,9 +1,11 @@
 from collections import defaultdict
+import concurrent.futures
+import functools
 import gzip
 import json
 import os
+from pathlib import Path
 import re
-from tqdm import tqdm
 
 
 FILE_DATE_PATTERN = re.compile(r'-(\d\d\d\d-\d\d-\d\d)\.')
@@ -11,8 +13,8 @@ FILE_DATE_PATTERN = re.compile(r'-(\d\d\d\d-\d\d-\d\d)\.')
 
 def read_json_lines(filepath, compressed=None):
     if compressed is None:
-        compressed = filepath.endswith('.gz')
-    
+        compressed = str(filepath).endswith('.gz')
+
     context = gzip.open(filepath, 'rt') if compressed else open(filepath)
     with context as f:
         for line in f:
@@ -20,22 +22,34 @@ def read_json_lines(filepath, compressed=None):
                 yield json.loads(line)
 
 
-def create_location_listings(location_file, id_file):
-    locations = {}
-    location_lookup = {}
-    for location in read_json_lines(location_file):
-        value = defaultdict(lambda: 0)
-        locations[location['id']] = value
-        location_lookup[location['id']] = value
+def deduplicate_locations(data, id_file):
+    """
+    Identify duplicate locations using a recent dump of the ``external_ids``
+    table and combine slot counts from locations that are duplicates. (This is
+    important because historical logs will contain reports for different
+    location IDs that we later learned were duplicates.)
+    """
+    clean = defaultdict(lambda: defaultdict(lambda: 0))
 
-
+    lookup = {}
     for row in read_json_lines(id_file):
+        location_id = row['provider_location_id']
+        if not (location_id in lookup):
+            lookup[location_id] = clean[location_id]
         if row['system'].startswith('univaf_'):
-            # print(f"Adding merged ID: {row['value']}")
-            location = locations[row['provider_location_id']]
-            location_lookup[row['value']] = location
-    
-    return locations, location_lookup
+            lookup[row['value']] = clean[location_id]
+
+    for location_id, dates in data.items():
+        clean_entry = lookup.get(location_id)
+        if clean_entry is None:
+            print(f"WARN: no matching row for: {location_id}")
+            clean_entry = clean[location_id]
+            lookup[location_id] = clean_entry
+
+        for day, count in dates.items():
+            clean_entry[day] = max(clean_entry[day], count)
+
+    return clean
 
 
 def count_capacity_slots(entry):
@@ -46,49 +60,76 @@ def count_capacity_slots(entry):
     return count
 
 
-def count_slots_by_day_in_locations(location_lookup, log_files):
-    for filepath in log_files:
-        file_name = os.path.basename(filepath)
-        file_date = FILE_DATE_PATTERN.search(filepath).group(1)
-        lines = tqdm(read_json_lines(filepath),
-                     mininterval=2,
-                     unit='rows',
-                     unit_scale=True,
-                     desc=f'Reading {file_name}')
-        for row in lines:
-            location = location_lookup.get(row['location_id'])
-            if location is None:
-                tqdm.write(f"WARN: no matching row for: {row['location_id']}")
+def summarize_slots(records, default_date=None):
+    """
+    Given an iterable of availability log records, count the total tracked slots
+    per location and day. Returns a dict where keys are location IDs and values
+    are a dict where keys are date strings (e.g. "2021-11-01") and values are
+    integers:
 
-            capacity = row.get('capacity')
-            slots = row.get('slots')
-            if capacity:
-                for entry in capacity:
-                    day = entry['date']
-                    count = count_capacity_slots(entry)
-                    location[day] = max(location[day], count)
-                    # Be extra careful
-                    if count > 5_000:
-                        tqdm.write(f"WARN: location {row['location_id']} has {count} slots on {day}")
-            elif slots:
-                count_by_day = defaultdict(lambda: 0)
-                for entry in slots:
-                    day = entry['start'][0:10]
-                    count = count_capacity_slots(entry)
-                    count_by_day[day] += count
-                for day, count in count_by_day.items():
-                    location[day] = max(location[day], count)
-                    # Be extra careful
-                    if count > 5_000:
-                        tqdm.write(f"WARN: location {row['location_id']} has {count} slots on {day}")
-            elif 'available' in row:
-                # If there's no day- or slot-level data, be conservative and
-                # count 1 slot for the current current day of the report.
-                # ONLY do this if we actual got *some* info about availability
-                # (i.e. 'available' is filled in) -- Ignore records that
-                # represent "no change" since that data will get filled in from
-                # reading other records.
-                location[file_date] = max(location[file_date], 1)
+        {
+          'location_id': {'2021-11-01': 15, '2021-11-02': 13, ...},
+          'location_id': {'2021-11-01': 28, '2021-11-02': 10, ...},
+          ...
+        }
+    """
+    locations = defaultdict(lambda: defaultdict(lambda: 0))
+    for row in records:
+        location_id = row['location_id']
+        location = locations[location_id]
+        capacity = row.get('capacity')
+        slots = row.get('slots')
+        if capacity:
+            for entry in capacity:
+                day = entry['date']
+                count = count_capacity_slots(entry)
+                location[day] = max(location[day], count)
+                # Be extra careful
+                if count > 5_000:
+                    print(f"WARN: location {location_id} has {count} slots on {day}")
+        elif slots:
+            count_by_day = defaultdict(lambda: 0)
+            for entry in slots:
+                day = entry['start'][0:10]
+                count = count_capacity_slots(entry)
+                count_by_day[day] += count
+            for day, count in count_by_day.items():
+                location[day] = max(location[day], count)
+                # Be extra careful
+                if count > 5_000:
+                    print(f"WARN: location {location_id} has {count} slots on {day}")
+        elif 'available' in row:
+            # If there's no day- or slot-level data, be conservative and
+            # count 1 slot for the current current day of the report.
+            # ONLY do this if we actual got *some* info about availability
+            # (i.e. 'available' is filled in) -- Ignore records that
+            # represent "no change" since that data will get filled in from
+            # reading other records.
+            day = default_date or entry['valid_at'][0:10]
+            location[day] = max(location[day], 1)
+
+    return locations
+
+
+def summarize_slots_in_file(file_path, cache_directory):
+    cache_path = cache_directory / f'{file_path.name}.json'
+    if cache_path.exists():
+        print(f'Reading cache: {cache_path}...')
+        with cache_path.open(encoding='utf-8') as f:
+            return json.load(f)
+    else:
+        print(f'Reading {file_path}...')
+        file_date = FILE_DATE_PATTERN.search(file_path.name).group(1)
+        result = summarize_slots(read_json_lines(file_path), file_date)
+
+        # Cache it for later use.
+        cache_path.parent.mkdir(parents=True, exist_ok=True)
+        with cache_path.open('w', encoding='utf-8') as f:
+            json.dump(result, f, separators=(',', ':'), ensure_ascii=False)
+
+        # Return a plain old dict of dicts so it's pickle-able.
+        return {location_id: dict(dates)
+                for location_id, dates in result.items()}
 
 
 def sum_slots_by_day(locations):
@@ -96,13 +137,13 @@ def sum_slots_by_day(locations):
     for counts in locations.values():
         for day, count in counts.items():
             days[day] += count
-    
+
     result = []
     total = 0
     for day in sorted(days.keys()):
         total += days[day]
         result.append({'date': day, 'count': days[day], 'total': total})
-    
+
     return result
 
 
@@ -111,13 +152,32 @@ if __name__ == '__main__':
     args = lib_cli.create_agument_parser().parse_args()
     dates = lib_cli.get_dates_in_range(args.start_date, args.end_date)
 
-    location_file = f'../data/univaf_raw/provider_locations-{dates[-1]}.ndjson'
-    id_file = f'../data/univaf_raw/external_ids-{dates[-1]}.ndjson'
-    log_files = [f'../data/univaf_raw/availability_log-{dt}.ndjson.gz' for dt in dates]
+    data_path = Path('../data/univaf_raw')
+    cache_path = Path('../data/univaf_counts')
 
-    locations, location_lookup = create_location_listings(location_file, id_file)
-    count_slots_by_day_in_locations(location_lookup, log_files)
+    # TODO: should this be yesterday, instead of the last date of the sequence?
+    id_file = data_path / f'external_ids-{dates[-1]}.ndjson'
+    log_files = [data_path / f'availability_log-{dt}.ndjson.gz' for dt in dates]
+
+    # FIXME: this needs to automatically download the relevant files.
+    
+    # Process each file, then combine the results into a dict of slot counts by
+    # day by location ID:
+    #   {
+    #     'location_id': {'2021-11-01': 15, '2021-11-02': 13, ...},
+    #     'location_id': {'2021-11-01': 28, '2021-11-02': 10, ...},
+    #     ...
+    #   }
+    locations = defaultdict(lambda: defaultdict(lambda: 0))
+    with concurrent.futures.ProcessPoolExecutor() as executor:
+        summarizer = functools.partial(summarize_slots_in_file, cache_directory=cache_path)
+        for summary in executor.map(summarizer, log_files):
+            for location_id, dates in summary.items():
+                for day, count in dates.items():
+                    locations[location_id][day] = max(locations[location_id][day], count)
+
+    locations = deduplicate_locations(locations, id_file)
 
     sums = sum_slots_by_day(locations)
     for day in sums:
-        print(f'{day["date"]}: {day["count"]:>7,d} (total: {day["total"]:>11,d})')
+        print(f'{day["date"]}: {day["count"]:>9,d} (total: {day["total"]:>11,d})')
