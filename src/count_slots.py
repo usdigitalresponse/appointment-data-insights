f2233c343bff9e2a406bc808273643d688a40191
Rob Brackett <rob@robbrackett.com>
2022-01-27 17:14:00 -0800

    Start with old, hacked-together script from last time

---
+++ src/count_slots.py
@@ -0,0 +1,230 @@
+# idfile = '../data/univaf_raw/external_ids-2021-10-28.ndjson'
+# locfile = '../data/univaf_raw/provider_locations-2021-10-28.ndjson'
+
+# logfiles = (
+#   '../data/univaf_raw/availability_log-2021-06-03.ndjson',
+#   '../data/univaf_raw/availability_log-2021-06-04.ndjson',
+#   '../data/univaf_raw/availability_log-2021-06-05.ndjson',
+#   '../data/univaf_raw/availability_log-2021-06-06.ndjson',
+#   '../data/univaf_raw/availability_log-2021-06-07.ndjson',
+#   '../data/univaf_raw/availability_log-2021-06-08.ndjson',
+#   '../data/univaf_raw/availability_log-2021-06-09.ndjson',
+#   '../data/univaf_raw/availability_log-2021-06-10.ndjson',
+#   '../data/univaf_raw/availability_log-2021-06-11.ndjson',
+#   '../data/univaf_raw/availability_log-2021-06-12.ndjson',
+#   '../data/univaf_raw/availability_log-2021-06-13.ndjson',
+#   '../data/univaf_raw/availability_log-2021-06-14.ndjson',
+#   '../data/univaf_raw/availability_log-2021-06-15.ndjson',
+#   '../data/univaf_raw/availability_log-2021-06-16.ndjson',
+#   '../data/univaf_raw/availability_log-2021-06-17.ndjson',
+#   '../data/univaf_raw/availability_log-2021-06-18.ndjson',
+#   '../data/univaf_raw/availability_log-2021-06-19.ndjson',
+#   '../data/univaf_raw/availability_log-2021-06-20.ndjson',
+#   '../data/univaf_raw/availability_log-2021-06-21.ndjson',
+#   '../data/univaf_raw/availability_log-2021-06-22.ndjson',
+#   '../data/univaf_raw/availability_log-2021-06-23.ndjson',
+#   '../data/univaf_raw/availability_log-2021-06-24.ndjson',
+#   '../data/univaf_raw/availability_log-2021-06-25.ndjson',
+#   '../data/univaf_raw/availability_log-2021-06-26.ndjson',
+#   '../data/univaf_raw/availability_log-2021-06-27.ndjson',
+#   '../data/univaf_raw/availability_log-2021-06-28.ndjson',
+#   '../data/univaf_raw/availability_log-2021-06-29.ndjson',
+#   '../data/univaf_raw/availability_log-2021-06-30.ndjson',
+#   '../data/univaf_raw/availability_log-2021-07-01.ndjson',
+#   '../data/univaf_raw/availability_log-2021-07-02.ndjson',
+#   '../data/univaf_raw/availability_log-2021-07-03.ndjson',
+#   '../data/univaf_raw/availability_log-2021-07-04.ndjson',
+#   '../data/univaf_raw/availability_log-2021-07-05.ndjson',
+#   '../data/univaf_raw/availability_log-2021-07-06.ndjson',
+#   '../data/univaf_raw/availability_log-2021-07-07.ndjson',
+#   '../data/univaf_raw/availability_log-2021-07-08.ndjson',
+#   '../data/univaf_raw/availability_log-2021-07-09.ndjson',
+#   '../data/univaf_raw/availability_log-2021-07-10.ndjson',
+#   '../data/univaf_raw/availability_log-2021-07-11.ndjson',
+#   '../data/univaf_raw/availability_log-2021-07-12.ndjson',
+#   '../data/univaf_raw/availability_log-2021-07-13.ndjson',
+#   '../data/univaf_raw/availability_log-2021-07-14.ndjson',
+#   '../data/univaf_raw/availability_log-2021-07-15.ndjson',
+#   '../data/univaf_raw/availability_log-2021-07-16.ndjson',
+#   '../data/univaf_raw/availability_log-2021-07-17.ndjson',
+#   '../data/univaf_raw/availability_log-2021-07-18.ndjson',
+#   '../data/univaf_raw/availability_log-2021-07-19.ndjson',
+#   '../data/univaf_raw/availability_log-2021-07-20.ndjson',
+#   '../data/univaf_raw/availability_log-2021-07-21.ndjson',
+#   '../data/univaf_raw/availability_log-2021-07-22.ndjson',
+#   '../data/univaf_raw/availability_log-2021-07-23.ndjson',
+#   '../data/univaf_raw/availability_log-2021-07-24.ndjson',
+#   '../data/univaf_raw/availability_log-2021-07-25.ndjson',
+#   '../data/univaf_raw/availability_log-2021-07-26.ndjson',
+#   '../data/univaf_raw/availability_log-2021-07-27.ndjson',
+#   '../data/univaf_raw/availability_log-2021-07-28.ndjson',
+#   '../data/univaf_raw/availability_log-2021-07-29.ndjson',
+#   '../data/univaf_raw/availability_log-2021-07-30.ndjson',
+#   '../data/univaf_raw/availability_log-2021-07-31.ndjson',
+#   '../data/univaf_raw/availability_log-2021-08-01.ndjson',
+#   '../data/univaf_raw/availability_log-2021-08-02.ndjson',
+#   '../data/univaf_raw/availability_log-2021-08-03.ndjson',
+#   '../data/univaf_raw/availability_log-2021-08-04.ndjson',
+#   '../data/univaf_raw/availability_log-2021-08-05.ndjson',
+#   '../data/univaf_raw/availability_log-2021-08-06.ndjson',
+#   '../data/univaf_raw/availability_log-2021-08-07.ndjson',
+#   '../data/univaf_raw/availability_log-2021-08-08.ndjson',
+#   '../data/univaf_raw/availability_log-2021-08-09.ndjson',
+#   '../data/univaf_raw/availability_log-2021-08-10.ndjson',
+#   '../data/univaf_raw/availability_log-2021-08-11.ndjson',
+#   '../data/univaf_raw/availability_log-2021-08-12.ndjson',
+#   '../data/univaf_raw/availability_log-2021-08-13.ndjson',
+#   '../data/univaf_raw/availability_log-2021-08-14.ndjson',
+#   '../data/univaf_raw/availability_log-2021-08-15.ndjson',
+#   '../data/univaf_raw/availability_log-2021-08-16.ndjson',
+#   '../data/univaf_raw/availability_log-2021-08-17.ndjson',
+#   '../data/univaf_raw/availability_log-2021-08-18.ndjson',
+#   '../data/univaf_raw/availability_log-2021-08-19.ndjson',
+#   '../data/univaf_raw/availability_log-2021-08-20.ndjson',
+#   '../data/univaf_raw/availability_log-2021-08-21.ndjson',
+#   '../data/univaf_raw/availability_log-2021-08-22.ndjson',
+#   '../data/univaf_raw/availability_log-2021-08-23.ndjson',
+#   '../data/univaf_raw/availability_log-2021-08-24.ndjson',
+#   '../data/univaf_raw/availability_log-2021-08-25.ndjson',
+#   '../data/univaf_raw/availability_log-2021-08-26.ndjson',
+#   '../data/univaf_raw/availability_log-2021-08-27.ndjson',
+#   '../data/univaf_raw/availability_log-2021-08-28.ndjson',
+#   '../data/univaf_raw/availability_log-2021-08-29.ndjson',
+#   '../data/univaf_raw/availability_log-2021-08-30.ndjson',
+#   '../data/univaf_raw/availability_log-2021-08-31.ndjson',
+#   '../data/univaf_raw/availability_log-2021-09-01.ndjson',
+#   '../data/univaf_raw/availability_log-2021-09-02.ndjson',
+#   '../data/univaf_raw/availability_log-2021-09-03.ndjson',
+#   '../data/univaf_raw/availability_log-2021-09-04.ndjson',
+#   '../data/univaf_raw/availability_log-2021-09-05.ndjson',
+#   '../data/univaf_raw/availability_log-2021-09-06.ndjson',
+#   '../data/univaf_raw/availability_log-2021-09-07.ndjson',
+#   '../data/univaf_raw/availability_log-2021-09-08.ndjson',
+#   '../data/univaf_raw/availability_log-2021-09-09.ndjson',
+#   '../data/univaf_raw/availability_log-2021-09-10.ndjson',
+#   '../data/univaf_raw/availability_log-2021-09-11.ndjson',
+#   '../data/univaf_raw/availability_log-2021-09-12.ndjson',
+#   '../data/univaf_raw/availability_log-2021-09-13.ndjson',
+#   '../data/univaf_raw/availability_log-2021-09-14.ndjson',
+#   '../data/univaf_raw/availability_log-2021-09-15.ndjson',
+#   '../data/univaf_raw/availability_log-2021-09-16.ndjson',
+#   '../data/univaf_raw/availability_log-2021-09-17.ndjson',
+#   '../data/univaf_raw/availability_log-2021-09-18.ndjson',
+#   '../data/univaf_raw/availability_log-2021-09-19.ndjson',
+#   '../data/univaf_raw/availability_log-2021-09-20.ndjson',
+#   '../data/univaf_raw/availability_log-2021-09-21.ndjson',
+#   '../data/univaf_raw/availability_log-2021-09-22.ndjson',
+#   '../data/univaf_raw/availability_log-2021-09-23.ndjson',
+#   '../data/univaf_raw/availability_log-2021-09-24.ndjson',
+#   '../data/univaf_raw/availability_log-2021-09-25.ndjson',
+#   '../data/univaf_raw/availability_log-2021-09-26.ndjson',
+#   '../data/univaf_raw/availability_log-2021-09-27.ndjson',
+#   '../data/univaf_raw/availability_log-2021-09-28.ndjson',
+#   '../data/univaf_raw/availability_log-2021-09-29.ndjson',
+#   '../data/univaf_raw/availability_log-2021-09-30.ndjson',
+#   '../data/univaf_raw/availability_log-2021-10-01.ndjson',
+#   '../data/univaf_raw/availability_log-2021-10-02.ndjson',
+#   '../data/univaf_raw/availability_log-2021-10-03.ndjson',
+#   '../data/univaf_raw/availability_log-2021-10-04.ndjson',
+#   '../data/univaf_raw/availability_log-2021-10-05.ndjson',
+#   '../data/univaf_raw/availability_log-2021-10-06.ndjson',
+#   '../data/univaf_raw/availability_log-2021-10-07.ndjson',
+#   '../data/univaf_raw/availability_log-2021-10-08.ndjson',
+#   '../data/univaf_raw/availability_log-2021-10-09.ndjson',
+#   '../data/univaf_raw/availability_log-2021-10-10.ndjson',
+#   '../data/univaf_raw/availability_log-2021-10-11.ndjson',
+#   '../data/univaf_raw/availability_log-2021-10-12.ndjson',
+#   '../data/univaf_raw/availability_log-2021-10-13.ndjson',
+#   '../data/univaf_raw/availability_log-2021-10-14.ndjson',
+#   '../data/univaf_raw/availability_log-2021-10-15.ndjson',
+#   '../data/univaf_raw/availability_log-2021-10-16.ndjson',
+#   '../data/univaf_raw/availability_log-2021-10-17.ndjson',
+#   '../data/univaf_raw/availability_log-2021-10-18.ndjson',
+#   '../data/univaf_raw/availability_log-2021-10-19.ndjson',
+#   '../data/univaf_raw/availability_log-2021-10-20.ndjson',
+#   '../data/univaf_raw/availability_log-2021-10-21.ndjson',
+#   '../data/univaf_raw/availability_log-2021-10-22.ndjson',
+#   '../data/univaf_raw/availability_log-2021-10-23.ndjson',
+#   '../data/univaf_raw/availability_log-2021-10-24.ndjson',
+#   '../data/univaf_raw/availability_log-2021-10-25.ndjson',
+#   '../data/univaf_raw/availability_log-2021-10-26.ndjson',
+#   '../data/univaf_raw/availability_log-2021-10-27.ndjson',
+#   '../data/univaf_raw/availability_log-2021-10-28.ndjson',
+# )
+
+import ndjson
+import json
+from collections import defaultdict
+import argparse
+import lib
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-s', '--start_date', help="first date to process")
+parser.add_argument('-e', '--end_date', help="last date to process")
+args = parser.parse_args()
+# parse dates
+dates = lib.parse_date(parser)
+
+id_file = f'../data/univaf_raw/external_ids-{dates[-1]}.ndjson'
+location_file = f'../data/univaf_raw/provider_locations-{dates[-1]}.ndjson'
+log_files = [f'../data/univaf_raw/availability_log-{dt}.ndjson' for dt in dates]
+
+
+locations = {}
+location_lookup = {}
+with open(location_file) as f:
+    reader = ndjson.reader(f)
+    for location in reader:
+        value = defaultdict(lambda: 0)
+        locations[location['id']] = value
+        location_lookup[location['id']] = value
+
+
+with open(id_file) as f:
+    reader = ndjson.reader(f)
+    for row in reader:
+        if row['system'].startswith('univaf_'):
+            # print(f"Adding merged ID: {row['value']}")
+            location = locations[row['provider_location_id']]
+            location_lookup[row['value']] = location
+
+for filepath in log_files:
+    print(f'Reading {filepath}')
+    with open(filepath) as f:
+        reader = ndjson.reader(f)
+        for row in reader:
+            location = location_lookup.get(row['location_id'])
+            if location is None:
+                print(f"WARN: no matching row for: {row['location_id']}")
+
+            capacity = row.get('capacity')
+            slots = row.get('slots')
+            if capacity:
+                for entry in capacity:
+                    # default_count = 0 if entry['available'].lower() == 'no' else 1
+                    # available_count = entry.get('available_count', default_count)
+                    # location[entry['date']] = max(location[entry['date']], available_count)
+                    name = entry['date']
+                    count = entry.get('available_count', 0) + entry.get('unavailable_count', 0)
+                    if count == 0:
+                        count = 1
+                    location[name] = max(location[name], count)
+            elif slots:
+                for entry in slots:
+                    name = entry['start'][0:10]
+                    count = entry.get('available_count', 0) + entry.get('unavailable_count', 0)
+                    if count == 0:
+                        count = 1
+                    location[name] = max(location[name], count)
+            else:
+                name = filepath[-17:-7]
+                location[name] = max(location[name], 1)
+
+total = 0
+for location_id, counts in locations.items():
+    for d, count in counts.items():
+        total += count
+
+print(f'TOTAL: {total}')
+# 436,493,577
